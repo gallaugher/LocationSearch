a9c00091ba817fcfc7425e35dfd9c8d42f521623
gallaugher <john.gallaugher@bc.edu>
2024-11-23 15:05:36 -0500

    Initial Commit

---
+++ LocationSearch/LocationSearchApp.swift
@@ -0,0 +1,14 @@
+// File: LocationSearchApp.swift Project: LocationSearch
+// Created by: Prof. John Gallaugher on 11/23/24
+// YouTube.com/profgallaugher  -  threads.net/john.gallaugher
+
+import SwiftUI
+
+@main
+struct LocationSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
